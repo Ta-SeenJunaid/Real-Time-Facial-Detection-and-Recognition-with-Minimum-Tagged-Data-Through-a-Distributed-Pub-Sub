3cdb4daf1f4c080d364e46608e5bfa0f76aa1365
Ta-SeenJunaid <taseen.junaid@gmail.com>
2021-12-20 10:47:52 +0100

    receiver.py

---
+++ receiver/receiver.py
@@ -0,0 +1,26 @@
+import threading
+
+import imagezmq
+
+
+class Receiver:
+
+    def __init__(self, hostname, port):
+        self.hostname = hostname
+        self.port = port
+        self._stop = False
+        self._data_ready = threading.Event()
+        self._thread = threading.Thread(target=self._run, args=())
+        self._thread.daemon = True
+        self._thread.start()
+
+    def _run(self):
+        receiver = imagezmq.ImageHub("tcp://{}:{}".format(self.hostname, self.port),
+                                     REQ_REP=False)
+        while not self._stop:
+            self._data = receiver.recv_jpg()
+            self._data_ready.set()
+        receiver.close()
+
+    def close(self):
+        self._stop = True
