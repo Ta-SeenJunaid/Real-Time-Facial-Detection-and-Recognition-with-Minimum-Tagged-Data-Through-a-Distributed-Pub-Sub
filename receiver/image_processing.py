421f7480ede1082a11e1217f053b44b97e6492b0
Ta-SeenJunaid <taseen.junaid@gmail.com>
2021-12-22 08:30:10 +0100

    find_encodings and mark attendance

---
+++ receiver/image_processing.py
@@ -0,0 +1,26 @@
+import datetime
+
+import cv2
+import face_recognition
+
+
+def find_encodings(images):
+    encode_list = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encode_list.append(encode)
+    return encode_list
+
+
+def mark_attendance(name):
+    with open('attendance.csv', 'r+') as f:
+        my_data_list = f.readlines()
+        name_list = []
+        for line in my_data_list:
+            entry = line.split(',')
+            name_list.append(entry[0])
+        if name not in name_list:
+            now = datetime.now()
+            dt_string = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dt_string}')
