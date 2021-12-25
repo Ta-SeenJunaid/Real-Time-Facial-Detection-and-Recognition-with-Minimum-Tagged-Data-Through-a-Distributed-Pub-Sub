7ca4c154a379ac2bc45d225eaec42d15088d8474
Ta-SeenJunaid <taseen.junaid@gmail.com>
2021-12-25 10:50:44 +0100

    process image

--- receiver/image_processing.py
+++ receiver/image_processing.py
@@ -2,6 +2,7 @@ import datetime
 
 import cv2
 import face_recognition
+import numpy as np
 
 
 def find_encodings(images):
@@ -24,3 +25,31 @@ def mark_attendance(name):
             now = datetime.now()
             dt_string = now.strftime('%H:%M:%S')
             f.writelines(f'\n{name},{dt_string}')
+
+
+def process_image(image, sender_name, encode_list_known, class_names):
+    image = cv2.flip(image, 1)
+
+    img_small = cv2.resize(image, (0,0), None, 0.25, 0.25)
+    img_small = cv2.cvtColor(img_small, cv2.COLOR_BGR2RGB)
+
+    faces_current_frame = face_recognition.face_locations(img_small)
+    encodes_current_frame = face_recognition.face_encodings(img_small, faces_current_frame)
+
+    for encode_face, face_loc in zip(encodes_current_frame, faces_current_frame):
+        matches = face_recognition.compare_faces(encode_list_known, encode_face)
+        face_dis = face_recognition.face_distance(encode_list_known, encode_face)
+        # print(face_dis)
+        match_index = np.argmin(face_dis)
+
+        if matches[match_index]:
+            name = class_names[match_index].upper()
+            # print(name)
+            y1, x2, y2, x1 = face_loc
+            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(image, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(image, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            mark_attendance(name)
+
+    cv2.imshow(sender_name, image)
