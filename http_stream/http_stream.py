4a810260dc09a718b56394370c562d1e165985f9
Ta-SeenJunaid <taseen.junaid@gmail.com>
2022-01-17 08:39:07 +0100

    receiver_ip arguments

--- http_stream/http_stream.py
+++ http_stream/http_stream.py
@@ -4,8 +4,9 @@ import cv2
 import imagezmq
 from werkzeug import Request, Response, run_simple
 
+receiver_ip = None
 
-def send_images_to_web(receiver_ip):
+def send_images_to_web():
     receiver = imagezmq.ImageHub(open_port=receiver_ip, REQ_REP= False)
     while True:
         sender_name, image = receiver.recv_image()
@@ -15,7 +16,7 @@ def send_images_to_web(receiver_ip):
 
 @Request.application
 def application(request):
-    return Response(send_images_to_web('tcp://192.168.0.101:5566'), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(send_images_to_web(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
@@ -35,5 +36,7 @@ if __name__ == '__main__':
                         )
 
     args = parser.parse_args()
+
+    receiver_ip = args.receiver_ip
 
     run_simple('192.168.0.101', 4000, application)
